f78cfa0f57af1475d8eefc920502b88805891237
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2022-02-11 19:24:00 +0530

    Add files via upload

---
+++ Section-3 Simple Client Project Page/pages/portfolio/[projectid].js
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+
+function PortfolioProjectPage() {
+    const router = useRouter();
+
+    console.log(router.pathname);
+    console.log(router.query);   
+    
+    // send a request to backend server to fetch data with an id of router.query.projectid
+
+    return (
+        <div>
+            <h1>The Portfolio Project Page</h1>
+        </div>
+    );
+}
+
+export default PortfolioProjectPage;
